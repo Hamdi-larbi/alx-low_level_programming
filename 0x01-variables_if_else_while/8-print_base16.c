3231126e721a4a461eb9091bca4ac4f71a2b9b35
Hamdi-larbi <larbi.hamdi@gmail.com>
2022-12-09 19:11:41 -0800

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print base 16 number
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	char h;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	for (h = 'a'; h <= 'f'; h++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
