fa0f6a169890122705d001d789cc140b723dacf0
Hamdi-larbi <larbi.hamdi@gmail.com>
2022-12-09 18:31:47 -0800

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints char below 10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
