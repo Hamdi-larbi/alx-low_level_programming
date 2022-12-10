30803d2275b3bd4394c52f15bc5a5b5ba74241fb
Hamdi-larbi <larbi.hamdi@gmail.com>
2022-12-09 18:19:05 -0800

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
