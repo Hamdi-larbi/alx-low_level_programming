3caa269be1d396b2c15d710361047411464133c6
Hamdi-larbi <larbi.hamdi@gmail.com>
2022-12-10 04:55:40 -0800

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry input
+ * Description: prints all possible different combinations of two digits
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int sum;
+
+	for (i = 0, i < 9, i++)
+	{
+		for (j = i + 1, j < 10, j++)
+		{
+			sum = i * 10 + j;
+			putchar(sum + '0');
+			if (sum < 90)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
