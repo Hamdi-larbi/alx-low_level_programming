f24a565e45ea40b435884e19b30af58fa92b38e2
Hamdi-larbi <larbi.hamdi@gmail.com>
2022-12-10 06:32:45 -0800

    update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints all possible combinations of two two-digit numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+	int sum;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = j + 1; l < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					sum = i * 100 + j * 100 + k * 10 + l;
+					if (sum < 9899)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
