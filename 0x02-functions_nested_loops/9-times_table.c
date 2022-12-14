4b8ec856bb45d7ab2760b48dd3486676e6b37966
Hamdi-larbi <larbi.hamdi@gmail.com>
2022-12-13 18:02:38 -0800

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,13 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (mult = 0; mult <= 9; mult++)
+		putchar( 0 + '0');
+		for (mult = 1; mult <= 9; mult++)
 		{
 			prod = num * mult;
 			if (prod < 10)
 			{
+				putchar(' ');
 				putchar(prod + '0');
 			}
 			else
