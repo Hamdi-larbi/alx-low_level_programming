fccb797473007a9a1b9abec59e63f130ceb3800e
Hamdi-larbi <larbi.hamdi@gmail.com>
2022-12-22 14:48:33 -0800

    update

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string into root13
+ * @s: string to encode
+ * Return: encoded sting
+ */
+
+char *rot13(char *s)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char n[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0, j;
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = n[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
